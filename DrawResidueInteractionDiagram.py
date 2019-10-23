33852d6138a71578a4f71562e76015da9b772269
Martin Rosellen <martin.rosellen@fu-berlin.de>
2019-10-23 15:09:20 +0100

    plot_residue function now returning array of coordinates of residues

--- DrawResidueInteractionDiagram.py
+++ DrawResidueInteractionDiagram.py
@@ -197,12 +197,16 @@ def draw_residue(ctx, x, y, text):
 
 
 def plot_residues(residue_plotting_coordinates, residue_names_to_plot, chain_column_id_mapping, ctx):
+    residue_coordinates = {}
     for col_id, coords in residue_plotting_coordinates.items():
         residue_names = [r for r in residue_names_to_plot if r[0] == chain_column_id_mapping[col_id]]
         residue_names = sorted(residue_names, key=lambda x: int(x[3:]))
         x = coords[0]
         for y, name in zip(coords[1], residue_names):
             draw_residue(ctx, x, y, name)
+            residue_coordinates[name] = [x, y]
+
+    return residue_coordinates
 
 
 def main(args):
@@ -240,7 +244,8 @@ def main(args):
     chain_column_id_mapping = selected_rows.drop_duplicates('Chain')[['Chain', 'Col']].set_index('Col').to_dict()[
         'Chain']
 
-    plot_residues(residue_plotting_coordinates, residue_names_to_plot, chain_column_id_mapping, ctx)
+    residue_coordinates = plot_residues(residue_plotting_coordinates, residue_names_to_plot, chain_column_id_mapping,
+                                        ctx)
 
     residue_interaction_tuples = get_residue_interaction_tuples(decomp_table_data_frame)
 
